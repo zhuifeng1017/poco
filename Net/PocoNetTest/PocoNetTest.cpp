c6b9726ea9c06eb0168e67e3130e40c3fdfcbc10
zhuifeng <zhuifeng1017@gmail.com>
2013-09-18 09:55:31 +0800

    000000

--- Net/PocoNetTest/PocoNetTest.cpp
+++ Net/PocoNetTest/PocoNetTest.cpp
@@ -30,8 +30,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	using namespace std;
 	using namespace Poco;
 	using namespace Poco::Net;
+//03de6c570bfe24bfc328ccd7ca46b76eadaf4334
+	//SHA1Engine engine;
+	//engine.update("abcde");
+	//cout << "_encryptDigest : " <<  SHA1Engine::digestToHex(engine.digest()) << endl;
 
-	CSocks5Client skClient("192.168.108.213", 7778, "03de6c570bfe24bfc328ccd7ca46b76eadaf4334");
+	CSocks5Client skClient("192.168.108.213", 7778, "abcde");
 	StreamSocket sk;
 	bool ret = skClient.GetSocket(sk);
 	if (!ret)
@@ -46,7 +50,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	}else
 	{
 		cout << "begin send data" << endl;
-		CSendRecvFile::sendFile(sk);
+		//const char *filePath = "D:\\ãë´¨µØÇø.mimg";
+		const char *filePath = "e:\\20130917085141.bmp";
+		CSendRecvFile::sendFile(sk, filePath);
 	}
 
 	sk.close();
